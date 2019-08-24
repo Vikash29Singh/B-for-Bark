fb7b5970ba4d7795a3de99f9d4efd6dc213829f0
koustabh69 <54432034+koustabh69@users.noreply.github.com>
2019-08-24 11:02:58 +0530

    Create searchd.php

---
+++ structure/searchd.php
@@ -0,0 +1,85 @@
+<html>
+
+<?php  include "conn_file.php"  ?>
+    
+<head>
+    
+    
+    </head>
+
+    <style>
+    
+    
+    </style>
+    
+    <body>
+    
+        <?php
+            $did=1;
+                $qry1="select * from disease_details where Disease_Id='$did'";
+                $q=mysqli_query($conn,$qry1);
+        $dis=mysqli_fetch_assoc($q);
+    
+    $qry2="select * from symptoms where Disease_Id='$did'";
+                $q2=mysqli_query($conn,$qry2);
+       
+    
+     
+    $qry3="select * from remedies where Disease_Id='$did'";
+                $q3=mysqli_query($conn,$qry3);
+       
+    ?>
+        
+        <h1><?php echo $dis['Disease_name'] ?></h1>
+        <h2>
+            <?php echo $dis['Disease_description'] ?>
+        </h2>
+        <br>
+        
+        <h2>
+            <u>SYMPTOMS</u>
+        </h2>
+        
+        <?php
+        $i=1;
+        while ($row1 = mysqli_fetch_assoc($q2)) 
+                    {
+                        echo $i.$row1["Symptoms"];
+                        $i=$i+1;
+                        ?><br><?php
+                    }
+        ?>
+        <marquee scrollamount="16" bgcolor="green"><span style="color:blue"><font size="13">This is OUR HELP</font></span></marquee>
+        
+        <h2><u>REMEDIES</u></h2>
+        <?php
+        $i=1;
+        while ($row2 = mysqli_fetch_assoc($q3)) 
+                    {
+                        echo $i.$row2["Remedies"];
+                        $i=$i+1;
+                        ?><br><?php
+                    }
+        ?>
+        
+        <!--<table border="3">
+        <tr rowspace="2">
+            <th>DOCTORS AVAILABLE</th>
+        </tr>
+            
+            <tr></tr>
+        
+        doctors available part would be dynamic and we need to work on it with a;; the API's then we can edit or create this part, do the rest for now
+        </table>-->
+        
+        
+    
+    
+    </body>
+
+
+
+</html>
+
+
+
