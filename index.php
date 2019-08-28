c4d53968d566367b60bd90295d07bbb7a0f2d715
koustabh69 <54432034+koustabh69@users.noreply.github.com>
2019-08-28 10:16:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,283 @@
+<!--File description-->
+<!--
+ File name:B for bark
+ Date:21/6/19
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Disease Recognition System</title>
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/fixed.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+
+
+</head>
+
+<body data-spy="scroll" data-target="#navbarResponsive">
+    <!--    home section-->
+    <div id="home">
+        <!--        navigation-->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top ">
+            <a class="navbar-brand" href="index.html"><img src="img/abc_new.png"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#features">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#resources">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#clients">Quotes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacts">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Sign-in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="UserRegistration.php">Sign-up</a>
+                    </li>
+                    <!--<li class="nav-item">
+                        <a class="nav-link" href="">Logout</a>
+                    </li>-->
+                </ul>
+            </div>
+        </nav>
+        <!-- end vavigation-->
+        <!-- start Landing page section-->
+        <div class="landing">
+            <div class="home-wrap">
+                <div class="home-inner">
+
+                </div>
+            </div>
+        </div>
+        <div class="caption text-center">
+            <h1 class="animated bounceInRight delay-2s">Welcome to <span style="color:#1ebba3;"> B for Bark</span></h1>
+            <h3 class="animated heartBeat delay-2s">The Only Pet Health centre</h3>
+            <a class="btn btn-outline-light btn-lg" href="#course">Get Started</a>
+        </div>
+        <!-- end landing page-->
+
+    </div>
+
+    <!--    end home section-->
+
+    <!--  course section-->
+    <div id="course" class="offset">
+        <div class="col-12 narrow text-center">
+            <h1>We know your concern for pets.</h1>
+            <p class="lead">The dogs should be friendly,outgoing,healthy,well-groomed and love to be fussed. All breeds, crossbreeds, shapes and sizes are welcome.</p>
+            <a class="btn btn-secondary btn-md" href="" target="_blank">B for Bark</a>
+        </div>
+
+    </div>
+
+    <!--    end course section-->
+
+
+    <!--  features section-->
+    <div id="features" class="offset">
+        <!--    start jumbotron-->
+        <div class="jumbotron">
+            <div class="narrow text-center">
+                <div class="col-12">
+                    <h3 class="heading">Features</h3>
+                    <div class="heading-underline"></div>
+                </div>
+                <div class="row text-center">
+
+
+                    <div class="col-md-4">
+                        <div class="feature">
+                            <i class="fas fa-play-circle fa-4x" data-fa-transform="shrink-3 up-5"></i>
+                            <h3>Trend Analysis</h3>
+                            <p>Recent Trends of the Dog Diseases.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-4">
+                        <div class="feature">
+                            <i class="fas fa-sliders-h fa-4x" data-fa-transform="shrink-4.5 up-4.5"></i>
+                            <h3>Availability</h3>
+                            <p>Availability of the Doctor Timings.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="feature">
+                            <i class="fab fa-wpforms fa-4x" data-fa-transform="shrink-4.5 up-4.5"></i>
+                            <h3>Health card</h3>
+                            <p>We provide the Health card for your pet.</p>
+                        </div>
+                    </div>
+                </div>
+                <!---- end row------>
+
+            </div>
+            <!--      end narrow-->
+        </div>
+        <!--    end jumbotron-->
+
+
+    </div>
+
+    <!--    end Features section-->
+
+    <!--  Resources section-->
+    <div id="resources" class="offset">
+
+        <div class="fixed-background">
+            <div class="row dark text-center">
+
+                <div class="col-12">
+                    <h3 class="heading">Built with care</h3>
+                    <div class="heading-underline"></div>
+                </div>
+                <div class="col-md-4">
+                    <h3>HELP</h3>
+                    <div class="feature">
+                        <i class="fab fa-hire-a-helper fa-3x"></i>
+                    </div>
+                    <p class="lead">We Help you to keep your pet Healthy</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>BRING</h3>
+                    <div class="feature">
+                        <i class="fas fa-bold fa-3x"></i>
+                    </div>
+                    <p class="lead">We Bring you the best services Available</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>CARE</h3>
+                    <div class="feature">
+                        <i class="fab fa-cuttlefish fa-3x"></i>
+                        <!--                font awesome icon-->
+                    </div>
+                    <p class="lead">We care and love your pets as ours.</p>
+                </div>
+            </div>
+            <!----end row dark--->
+            <div class="fixed-wrap">
+                <div class="fixed">
+                </div>
+            </div>
+        </div>
+        <!----end fixed background--->
+    </div>
+    <!-- end Resources section-->
+    <!--  clients section-->
+    <div id="clients" class="offset">
+        <!--    start jumbotron-->
+        <div class="jumbotron">
+
+            <div class="col-12">
+                <h3 class="heading">Quotes</h3>
+                <div class="heding-underline"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 clients">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/wh3.jpg">
+                        </div>
+                        <div class="col-md-8">
+                            <blockquote>
+                                <i class="fas fa-quote-left"></i>
+                                Dogs are a tremendous gift to the world! They are amazingly sensitive, intuitive creatures.B for Bark is passionate about working towards it.
+                                <hr class="clients-hr">
+                                <cite>&#8212;Eric,small bussiness Owner</cite>
+
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 clients">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/wh4.jpg">
+                        </div>
+                        <div class="col-md-8">
+                            <blockquote>
+                                <i class="fas fa-quote-left"></i>
+                                Humans love their pet dogs in the same way as they do their children. B for Bark Equally love your Dogs and will ensure their Good Health.
+                                <hr class="clients-hr">
+                                <cite>&#8212;Rachel,Professional photogarpher</cite>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <!---end row----->
+        </div>
+        <!---end jumbotron---->
+        <div class="col-12 narrow text-center">
+            <p class="lead"> Want to Know more about B for Bark? So Sign-In</p>
+            <a class="btn btn-secondary btn-md" href="" target="_blank">B for Bark</a>
+        </div>
+    </div>
+    <!--   end clients section-->
+
+
+    <!--  contacts section-->
+    <div id="contacts" class="offset">
+        <footer>
+            <div class="row justify-content-center">
+                <div class="col-md-5 text-center">
+                    <img src="img/abc_new.png">
+
+                    <p>Owning a dog is a joy, privilege, and responsibility. B for Bark is committed to protecting the health and well-being of all dogs.</p>
+                    <strong>Contact Info</strong>
+                    <p>(888) 888-8888<br>email@bforbark.com</p>
+                    <a href="" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                <hr class="socket">
+                &copy;B for Bark.
+            </div>
+        </footer>
+    </div>
+    <!--   end contacts section-->
+    <!--- Script Source Files -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
+    <!--- End of Script Source Files -->
+    
+    
+    
+<script type="text/javascript">
+ var vsid = "sa33363";
+ (function() { 
+ var vsjs = document.createElement('script'); vsjs.type = 'text/javascript'; vsjs.async = true; vsjs.setAttribute('defer', 'defer');
+  vsjs.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.virtualspirits.com/vsa/chat-'+vsid+'.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vsjs, s);
+ })();
+</script>
+    
+    
+    
+
+</body>
+
+</html>
