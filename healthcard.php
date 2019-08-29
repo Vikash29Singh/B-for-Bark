d95e6c1e7e3ee3455b53dce71783fbe4bc2c965d
jp2948 <54465191+jp2948@users.noreply.github.com>
2019-08-29 11:34:25 +0530

    Update healthcard.php

--- healthcard.php
+++ healthcard.php
@@ -107,14 +107,14 @@
     <div id="home">
         <!--        navigation-->
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top ">
-            <a class="navbar-brand" href="index.html">Welcome to B for Bark</a>
+            <a class="navbar-brand" href="index.php">Welcome to B for Bark</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <!-- <li>
             <li class="nav-item">
