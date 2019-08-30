4466693adadb084b210b74209a41c89168af6315
koustabh69 <54432034+koustabh69@users.noreply.github.com>
2019-08-31 00:09:08 +0530

    Update recomend.php

--- structure/recomend.php
+++ structure/recomend.php
@@ -11,21 +11,16 @@
     
         <?php
         
-            $qry2="SELECT COUNT(Disease_Id) FROM d_search GROUP BY Disease_Id ORDER BY COUNT(Disease_Id) DESC LIMIT 3";
+            $qry2="SELECT Disease_Id FROM d_search GROUP BY Disease_Id ORDER BY COUNT(Disease_Id) DESC LIMIT 2";
         
             $ret=mysqli_query($conn,$qry2);
             //$my=mysqli_fetch_assoc($ret);
             
         while (  $row =  mysqli_fetch_assoc($ret)    )
             {
-                /*$projects[] = $row;*/
-            
-                //echo $row[0];
-            
-                //echo $row['Disease_Id'];
                 foreach ($row as $row)
                 {
-                    echo $row;
+                    echo $row." ";
                 }
             }
       ?>
