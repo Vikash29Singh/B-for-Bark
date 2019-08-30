4ef0b5c80cc239136b2a77e87658227e8f36cb69
koustabh69 <54432034+koustabh69@users.noreply.github.com>
2019-08-30 23:51:03 +0530

    Update recomend.php

--- structure/recomend.php
+++ structure/recomend.php
@@ -10,11 +10,29 @@
     <body>
     
         <?php
-            $qry="SELECT TOP 1 FROM  ( SELECT Disease_Id, COUNT(Disease_Id) as "DiseaseCount" FROM d_search GROUP BY Disease_Id ORDER BY "DiseaseCount" DESC )";
         
-            $ret=mysqli_query($conn,$qry);
-            $my=mysqli_fetch_assoc($ret);
-        ?>
+            $qry2="SELECT COUNT(Disease_Id) FROM d_search GROUP BY Disease_Id ORDER BY COUNT(Disease_Id) DESC LIMIT 3";
+        
+            $ret=mysqli_query($conn,$qry2);
+            //$my=mysqli_fetch_assoc($ret);
+            
+        while (  $row =  mysqli_fetch_assoc($ret)    )
+            {
+                /*$projects[] = $row;*/
+            
+                //echo $row[0];
+            
+                //echo $row['Disease_Id'];
+                foreach ($row as $row)
+                {
+                    echo $row;
+                }
+            }
+      ?>
+        
+            
+        
+        
     
     </body>
 
