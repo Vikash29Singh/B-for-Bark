627975af1cfbfa7254c88041e862b17d159176b6
jp2948 <54465191+jp2948@users.noreply.github.com>
2019-08-30 20:33:56 +0530

    Create mail_configuration.php

---
+++ mail_configuration.php
@@ -0,0 +1,11 @@
+<?php
+define("PROJECT_HOME","http://localhost/B-for-Bark/");
+
+define("PORT", "587"); // port number
+define("MAIL_USERNAME", "jaiprakash.advani@mca.christuniversity.in"); // smtp usernmae
+define("MAIL_PASSWORD", "90972048"); // smtp password
+define("MAIL_HOST", "smtp.gmail.com"); // smtp host
+define("MAILER", "smtp");
+define("SENDER_NAME", "ADMIN");
+define("SERDER_EMAIL", "jaiprakash.advani@mca.christuniversity.in");
+?>
