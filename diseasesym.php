68437c1b855c07c21351d4775092ff867693ae5b
Nithya.V <nithya.v@mca.christuniversity.in>
2019-09-29 20:34:38 +0530

    Update diseasesym.php

--- diseasesym.php
+++ diseasesym.php
@@ -114,9 +114,11 @@
                     <li class="nav-item">
                         <a class="nav-link" href="petregister.php">Pet-Register</a>
                     </li>
+<!--
                     <li class="nav-item">
                         <a class="nav-link" href="">History</a>
                     </li>
+-->
                     <li class="nav-item">
                         <a class="nav-link" href="logout.php">Logout</a>
                     </li>
